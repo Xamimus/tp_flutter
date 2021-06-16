30ce83e3a33754b479243bc10fb6e27be1fd1668
Nathan PIGNON <nathan.pignon79@gmail.com>
2021-06-16 11:01:47 +0200

    Container deatils

--- lib/containers/Details/Details.dart
+++ lib/containers/Details/Details.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tp/components/vertical_scrollable_bar.dart';
+//import 'package:flutter_tp/components/vertical_scrollable_bar.dart';
 
 class Details extends StatelessWidget {
   Details({Key? key}) : super(key: key);
@@ -16,15 +16,49 @@ class Details extends StatelessWidget {
               image: new DecorationImage(image: new AssetImage("images/test.jpg"), fit: BoxFit.cover,),
             ),
           ),
-          new Center(
+          Align(
+            alignment: Alignment(-0.9, -0.95),
             child: IconButton(
-              color: Colors.white,
-              icon: Text("Test"),
+              icon: new Icon(Icons.arrow_back, color: Colors.white),
               onPressed: () {
                 print("Pressed");
               }
             ),
-          )
+          ),
+          Align(
+            alignment: Alignment(0.9, -0.95),
+            child: IconButton(
+              icon: new Icon(Icons.shopping_cart, color: Colors.white),
+              onPressed: () {
+                print("Pressed");
+              }
+            ),
+          ),
+          // Black square centered in stack
+          Align(
+            alignment: Alignment(0.0, 1.0),
+            child: new Container(
+              height: 250.0,
+              color: Colors.transparent,
+              child: new Container(
+                decoration: new BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: new BorderRadius.only(
+                    topLeft: const Radius.circular(50.0),
+                    topRight: const Radius.circular(50.0),
+                  )
+                ),
+              alignment: Alignment(0.0, -0.65) ,
+              child: new Text(
+                "Nom du produit à rallonge pour voir sur plusieurs ligne",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+                ),
+              )
+            ),
+          ),
         ],
       )
     );
